16eca5a6833403d7edddc3eaf498dda0161859b4
Jeroen Schot <jeroen.schot@surfsara.nl>
2014-03-08 19:35:46 +0100

    Remove class variables
    The class variables are not needed because we are using instance
    variables. Also remove the underscore prefixes to improve readability.

--- topos.py
+++ topos.py
@@ -20,35 +20,30 @@ DEFAULT_SERVER = 'https://topos.grid.sara.nl/4.1'
 
 class Server:
     """Represents a ToPoS server instance. Only used to create Pool objects."""
-    _server = None
 
     def __init__(self, server=DEFAULT_SERVER):
-        self._server = server
+        self.server = server
 
     def __getitem__(self, pool):
         """Return a Pool object with the given pool name."""
-        return Pool(pool=pool, server=self._server)
+        return Pool(pool=pool, server=self.server)
 
     def new_pool(self):
         """Request a new pool from the server."""
-        r = requests.get("{}/newPool".format(self._server))
-        print r.status_code
+        r = requests.get("{}/newPool".format(self.server))
         if r.status_code != 200:
             raise RuntimeError("error getting new pool")
         pool = r.url.split('/')[-2]
-        return self.__getitem__(pool)
+        return self[pool]
 
 
 class Pool:
 
-    _pool = None
-    _server = None
-    _timeout = None
-    _autorefresh = None
-
     def __init__(self, pool, server=DEFAULT_SERVER):
-        self._server = server
-        self._pool = pool
+        self.server = server
+        self.pool = pool
+        timeout = 0
+        autorefresh = False
 
     def __iter__(self):
         return self
@@ -56,25 +51,25 @@ class Pool:
     def next(self):
         """Fetch a new token."""
         params = {}
-        if (self._timeout > 0):
-            params['timeout'] = self._timeout
+        if (self.timeout > 0):
+            params['timeout'] = self.timeout
         r = requests.get("{}/pools/{}/nextToken"
-            .format(self._server, self._pool),
-            params={'timeout': self._timeout})
+            .format(self.server, self.pool),
+            params={'timeout': self.timeout})
         if r.status_code == 404:
             raise StopIteration
         orig_headers = r.history[0].headers
         token = {}
-        token["id"] = str(r.url.split('/')[-1])
+        token["id"] = r.url.split('/')[-1]
         token["data"] = r.content
-        if (self._timeout > 0):
+        if (self.timeout > 0):
             token["lock"] = orig_headers["x-topos-lockurl"]
         return token
 
     def remove(self, token):
         """Remove token from the pool."""
         r = requests.delete("{}/pools/{}/tokens/{}"
-            .format(self._server, self._pool, token['id']))
+            .format(self.server, self.pool, token['id']))
         if r.status_code == 404:
             raise KeyError
 
@@ -87,8 +82,8 @@ class Pool:
                        corresponding token is deleted or unlocked
                        (not yet implemented)
         """
-        self._timeout = timeout
-        self._autorefresh = autorefresh
+        self.timeout = timeout
+        self.autorefresh = autorefresh
 
     def unlock(self, token):
         """Unlock token, allowing others to process it."""
